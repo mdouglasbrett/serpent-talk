f1f312a42e8df39bfca9626ed19f87eaa2c320a9
Michael Brett <mdouglasbrett@icloud.com>
2017-11-27 13:29:15 +0000

    Add serpent-talk and -main functions
    Note: println doesn't seem to give me terminal output at this point

--- src/serpent_talk/talk.clj
+++ src/serpent_talk/talk.clj
@@ -1,4 +1,9 @@
 (ns serpent-talk.talk
   (:require [camel-snake-kebab.core :as csk]))
 
-(csk/->snake_case "hello pigeon")
+(defn serpent-talk [input]
+  (str "Serpent! You said: "
+       (csk/->snake_case input)))
+
+(defn -main [& args]
+  println (serpent-talk (first args)))
