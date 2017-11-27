6cf2f66081362f93a89d40c9933860e92a0c9699
Michael Brett <mdouglasbrett@icloud.com>
2017-11-27 09:03:44 +0000

    Add camel-snake-kebab dependency

--- src/serpent_talk/talk.clj
+++ src/serpent_talk/talk.clj
@@ -1,6 +1,4 @@
-(ns serpent-talk.talk)
+(ns serpent-talk.talk
+  (:require [camel-snake-kebab.core :as csk]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(csk/->snake_case "hello pigeon")
