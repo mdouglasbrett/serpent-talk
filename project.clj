3ca221d250caff3e6d57a9bde983483444f01e33
Michael Brett <mdouglasbrett@icloud.com>
2017-11-28 18:24:03 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject serpent-talk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Living Clojure example of using libraries"
+  :url "https://github.com/mdouglasbrett/serpent-talk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
